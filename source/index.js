d239f1c0d59c799eb50e728e5afe0ee10c11cd39
Javier Garmón <jgarmon@inevio.com>
2019-07-24 14:31:45 +0200

    wip

--- source/index.js
+++ source/index.js
@@ -1,23 +1,31 @@
 'use strict'
 
-const fs = require('fs')
+const fs = require('fs').promises
 const path = require('path')
 const AdBlockClient = require('adblock-rs')
 
-const client = new AdBlockClient.Engine([], false)
-
-//const file = path.resolve(__dirname, 'detector.buffer');
-
 module.exports.initialize = async () => {
-  /*new Promise((resolve, reject) => {
-fs.readFile(file, (err, buffer) => {
-  if (err) { return reject(err); }
-  client.deserialize(buffer);
-  return resolve();
-});
-});*/
-}
+  let rules = []
+  let lists = await Promise.all([
+    fs.readFile(path.join(__dirname, '../lists/easylist.txt'), 'utf8'),
+    fs.readFile(path.join(__dirname, '../lists/unbreak.txt'), 'utf8'),
+    fs.readFile(path.join(__dirname, '../lists/miners.txt'), 'utf8')
+  ])
 
-module.exports.containsAds = (url, type, debug) => {
-  client.check(url, type, debug)
+  lists.forEach(list => { rules = rules.concat(list.split('\n')) })
+
+  const client = new AdBlockClient.Engine(rules, false)
+  client.updateResources(await fs.readFile(path.join(__dirname, '../lists/resources.txt'), 'utf8'))
+
+  lists = null
+  rules = null
+
+  return client
 }
+/*
+module.exports.initialize().then(client => {
+  console.log(client.check('http://example.com/-advertisement-icon.', 'http://example.com/helloworld', 'image'))
+  console.log(client.check('http://example.com/-advertisement-icon.', 'http://example.com/helloworld', 'image'))
+  console.log(client.check('https://bbci.co.uk/test/analytics.js', 'https://bbc.co.uk', 'script'))
+})
+*/
